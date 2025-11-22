1463e1b6b48c54333295e022f52f8c54034f9ac3
Bruno Corso Viera <brunocorsovieira@gmail.com>
2025-11-22 15:58:51 -0300

    updated table name

--- nodejs_api/src/db/schema.ts
+++ nodejs_api/src/db/schema.ts
@@ -1,7 +1,7 @@
 
 import { pgTable, integer, varchar} from "drizzle-orm/pg-core";
 
-export const users = pgTable("Users", {
+export const users = pgTable("users", {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
     name: varchar({length: 255}).notNull(),
     email: varchar({length: 255}).notNull(),
